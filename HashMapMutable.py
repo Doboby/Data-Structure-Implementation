eb2d4a3df8596b17738261f843e638957594ecdb
dumei <meidoumian@qq.com>
2022-04-25 16:47:46 +0800

    implement all function

--- HashMapMutable.py
+++ HashMapMutable.py
@@ -9,42 +9,51 @@ class Node:
 class HashMap(object):
     empty = object()
 
-    def __init__(self, hashcode=13):
+    def __init__(self, hashcode=51):
         self.key_set = []
         self.data = [self.empty for _ in range(hashcode)]
         self.size = hashcode
         self.len = 0
+        self.index = 0
 
     def __len__(self):
         return self.len
 
     def add(self, key, value):
-
-        hash_value = hash(key) % self.size
-        kv_entry = Node(key, value)
-
-        if self.data[hash_value] == self.empty \
-                or self.data[hash_value].key == -1:
-            self.data[hash_value] = kv_entry
-            self.key_set.append(key)
-            self.len = self.len + 1
+        if key in self.key_set:
+            for temp in self.data:
+                if temp != self.empty and temp.key == key:
+                    temp.value = value
+                    return
         else:
-            i = 0
-            flag = False
-            while i < self.size:
-                index = (hash_value + 1) % self.size
-                hash_value = index
-                if self.data[index] == self.empty \
-                        or self.data[hash_value].key == -1:
-                    self.data[index] = kv_entry
-                    self.len = self.len + 1
-                    flag = True
-                    break
-                else:
-                    hash_value += 1
-                    i += 1
-            if not flag:
-                print("no space")
+            hash_value = hash(key) % self.size
+            kv_entry = Node(key, value)
+
+            if self.data[hash_value] == self.empty \
+                    or self.data[hash_value].key == -1:
+                self.data[hash_value] = kv_entry
+                self.key_set.append(key)
+                self.len = self.len + 1
+            else:
+                i = 0
+                flag = False
+                while i < self.size:
+                    index = (hash_value + 1) % self.size
+                    hash_value = index
+                    if self.data[index] == self.empty \
+                            or self.data[hash_value].key == -1:
+                        self.data[index] = kv_entry
+                        self.key_set.append(key)
+                        self.len = self.len + 1
+                        return True
+                    else:
+                        i += 1
+
+    def find_in_key_set(self, value):
+        for i in self.key_set:
+            if i == value:
+                return True
+        return False
 
     def get(self, key):
         hash_value = hash(key) % self.size
@@ -74,7 +83,7 @@ class HashMap(object):
         print("no element")
         return -1
 
-    def remove(self, key):
+    def remove_by_key(self, key):
         hash_value = self.get_hash_value(key)
         if hash_value == -1:
             return False
@@ -84,3 +93,107 @@ class HashMap(object):
             self.key_set.remove(key)
             self.len = self.len - 1
             return True
+
+    def remove_by_index(self, index):
+        if index < self.size:
+            key = self.data[index].key
+            if key == -1 or self.data[index] == self.empty:
+                print("no element")
+                return False
+            else:
+                self.data[index].key = -1
+                self.key_set.remove(key)
+                self.len = self.len - 1
+                return True
+        else:
+            print("out of bounds")
+            return False
+
+    def get_size(self):
+        size = len(self.key_set)
+        return size
+
+    def to_kv_entry_list(self):
+        list = []
+        for key in self.key_set:
+            list.append(Node(key, self.get(key)))
+        return list
+
+    def from_list(self, list):
+        for k, v in enumerate(list):
+            self.add(k, v)
+
+    def to_list(self):
+        list = []
+        for key in self.key_set:
+            list.append(self.get(key))
+        return list
+
+    def from_dict(self, dict):
+        for k, v in dict.items():
+            self.add(k, v)
+
+    def to_dict(self):
+        kvDict = {}
+        if self.len is 0:
+            return kvDict
+        else:
+            for temp in self.data:
+                if temp != self.empty and temp.key != -1:
+                    kvDict[temp.key] = temp.value
+        return kvDict
+
+    def map(self, f):
+        dict = {}
+        for key in self.key_set:
+            value = f(self.get(key))
+            dict[key] = value
+        return dict
+
+    def mempty(self):
+        return None
+
+    def mconcat(self, a, b):
+        if a is None:
+            return b
+        if b is None:
+            return a
+        for key in b.key_set:
+            value = b.get(key)
+            a.add(key, value)
+        return a
+
+    def find_iseven(self):
+        list = self.to_list()
+        my_list = []
+        for value in list:
+            if type(value) is int or type(value) is float:
+                if value % 2 == 0:
+                    my_list.append(value)
+        return my_list
+
+    def filter_iseven(self):
+        list = self.to_list()
+        for value in list:
+            if type(value) is int or type(value) is float:
+                if value % 2 == 0:
+                    list.remove(value)
+        return list
+
+    def reduce(self, f, initial_state):
+        state = initial_state
+        for key in self.key_set:
+            value = self.get(key)
+            state = f(state, value)
+        return state
+
+    def __iter__(self):
+        return iter(self.to_kv_entry_list())
+
+    def __next__(self):
+        if self.index >= self.len:
+            raise StopIteration("end of hashmap")
+        else:
+            self.index += 1
+            val = self.get(self.key_set[self.index - 1])
+            return val
--- HashMapMutable_test.py
+++ HashMapMutable_test.py
@@ -1,5 +1,6 @@
-
 import unittest
+from hypothesis import given
+import hypothesis.strategies as st
 from HashMapMutable import HashMap
 
 
@@ -31,5 +32,133 @@ class TestHashMapMutable(unittest.TestCase):
         hash.add(8, 123)
         hash.add(11, 44)
         hash.add(14, 66)
-        hash.remove(8)
-        self.assertEqual(len(hash), 3)
+        hash.remove_by_key(8)
+        self.assertEqual(hash.get_size(), 3)
+
+    def test_from_dict(self):
+        hash = HashMap()
+        dict = {1: 2, 2: 4, 3: 6, 4: 8}
+        hash.from_dict(dict)
+        self.assertEqual(hash.get(4), 8)
+        self.assertEqual(hash.get(3), 6)
+
+    def test_to_dict(self):
+        hash = HashMap()
+        hash.add(1, 2)
+        hash.add(2, 3)
+        hash.add(3, 2)
+        hash.add(5, 1)
+        hash.to_dict()
+        self.assertEqual(hash.to_dict(), {1: 2, 3: 2, 5: 1, 2: 3})
+
+    def test_get_size(self):
+        hash = HashMap()
+        self.assertEqual(hash.get_size(), 0)
+        hash.add(1, 2)
+        self.assertEqual(hash.get_size(), 1)
+        hash.add(14, 2)
+        self.assertEqual(hash.get_size(), 2)
+        hash.add(1, 3)
+        self.assertEqual(hash.get_size(), 2)
+
+    def test_to_list(self):
+        hash = HashMap()
+        dict = {4: 2, 3: 2, 5: 1, 1: 3}
+        hash.from_dict(dict)
+        self.assertEqual(hash.to_list(), [2, 2, 1, 3])
+
+    def test_from_list(self):
+        test_data = [
+            [],
+            ['a'],
+            ['0', '11'],
+            [1, 2, 3],
+            [None]
+        ]
+        for e in test_data:
+            hash = HashMap()
+            hash.from_list(e)
+            self.assertEqual(hash.to_list(), e)
+
+    def test_find_iseven(self):
+        hash = HashMap()
+        hash.from_list([1, 2, 3, 4, 5, 6.0, 7.0, None, 'ss', 'dasd'])
+        self.assertEqual(hash.to_list(), [1, 2, 3, 4, 5, 6.0, 7.0, None, 'ss', 'dasd'])
+        self.assertEqual(hash.find_iseven(), [2, 4, 6.0])
+
+    def test_filter_iseven(self):
+        hash = HashMap()
+        hash.from_list([1, 2, 3, 4, 5, 6.0, 7.0, None, 'ss', 'dasd'])
+        self.assertEqual(hash.to_list(), [1, 2, 3, 4, 5, 6.0, 7.0, None, 'ss', 'dasd'])
+        self.assertEqual(hash.filter_iseven(), [1, 3, 5, 7.0, None, 'ss', 'dasd'])
+
+    def test_map(self):
+        dict1 = {3: 23, 4: 323}
+        dict2 = {3: '23', 4: '323'}
+        hash = HashMap()
+        hash.from_dict(dict1)
+        self.assertEqual(hash.map(str), dict2)
+
+    def test_reduce(self):
+        hash = HashMap()
+        self.assertEqual(hash.reduce(lambda st, e: st + e, 0), 0)
+        dict1 = {3: 23, 4: 323}
+        hash1 = HashMap()
+        hash1.from_dict(dict1)
+        self.assertEqual(hash1.reduce(lambda st, e: st + e, 0), 346)
+
+    def test_iter(self):
+        dict1 = {1: 123, 2: 333, 3: 23, 4: 323}
+        table = HashMap()
+        table.from_dict(dict1)
+        tmp = {}
+        for e in table:
+            tmp[e.key] = e.value
+        self.assertEqual(table.to_dict(), tmp)
+        i = iter(HashMap())
+        self.assertRaises(StopIteration, lambda: next(i))
+
+    @given(a=st.lists(st.integers()))
+    def test_monoid_identity(self, a):
+        hash = HashMap()
+        hash_a = HashMap()
+        hash_a.from_list(a)
+        self.assertEqual(hash.mconcat(hash.mempty(), hash_a), hash_a)
+        self.assertEqual(hash.mconcat(hash_a, hash.mempty()), hash_a)
+
+    @given(a=st.lists(st.integers()), b=st.lists(st.integers()), c=st.lists(st.integers()))
+    def test_monoid_associativity(self, a, b, c):
+        hash = HashMap()
+        hash_a = HashMap()
+        hash_b = HashMap()
+        hash_c = HashMap()
+        # add list to HashMap
+        hash_a.from_list(a)
+        hash_b.from_list(b)
+        hash_c.from_list(c)
+        # (a·b)·c
+        a_b = hash.mconcat(hash_a, hash_b)
+        ab_c = hash.mconcat(a_b, hash_c)
+        # a·(b·c)
+        b_c = hash.mconcat(hash_b, hash_c)
+        a_bc = hash.mconcat(hash_a, b_c)
+        self.assertEqual(ab_c, a_bc)
+
+    @given(st.lists(st.integers()))
+    def test_from_list_to_list_equality(self, a):
+        hash = HashMap()
+        hash.from_list(a)
+        b = hash.to_list()
+        self.assertEqual(a, b)
+
+    @given(st.lists(st.integers()))
+    def test_python_len_and_list_size_equality(self, a):
+        hash = HashMap()
+        hash.from_list(a)
+        self.assertEqual(hash.get_size(), len(a))
+
+    @given(st.lists(st.integers()))
+    def test_from_list(self, a):
+        hash = HashMap()
+        hash.from_list(a)
+        self.assertEqual(hash.to_list(), a)
